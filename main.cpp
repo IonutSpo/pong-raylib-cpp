af211b12d5360997d2eea02932b5c6f88be3da79
IonutSpo <ionutklrspo@gmail.com>
2022-06-16 10:14:11 +0300

    Draw paddles and ball on screen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,65 @@
 #include "raylib.h"
 
+struct AnimData
+{   
+    Texture2D texture;
+    Rectangle rec;
+    Vector2 pos;
+    int frame;
+    float updateTime;
+    float runningTime;
+};
+
 int main() 
-{
-    return 0;
+{   
+
+    const int windowWidth{600};
+    const int windowHeight{300};
+    InitWindow(windowWidth, windowHeight, "Pong");
+
+    AnimData ball;
+    ball.texture = LoadTexture("textures/ball.png");
+    ball.rec.width = ball.texture.width / 6;
+    ball.rec.height = ball.texture.height;
+    ball.rec.x = 0;
+    ball.rec.y = 0;
+    ball.pos.x = windowWidth / 2 - ball.rec.width / 2;
+    ball.pos.y = windowHeight / 2 - ball.rec.height / 2;
+
+    AnimData leftPaddle;
+    leftPaddle.texture = LoadTexture("textures/left_paddle.png");
+    leftPaddle.rec.width = leftPaddle.texture.width;
+    leftPaddle.rec.height = leftPaddle.texture.height;
+    leftPaddle.rec.x = 0;
+    leftPaddle.rec.y = 0;
+    leftPaddle.pos.x = 0;
+    leftPaddle.pos.y = windowHeight / 2 - leftPaddle.rec.height / 2;
+
+    AnimData rightPaddle;
+    rightPaddle.texture = LoadTexture("textures/right_paddle.png");
+    rightPaddle.rec.width = rightPaddle.texture.width;
+    rightPaddle.rec.height = rightPaddle.texture.height;
+    rightPaddle.rec.x = 0;
+    rightPaddle.rec.y = 0;
+    rightPaddle.pos.x = windowWidth - rightPaddle.rec.width;
+    rightPaddle.pos.y = windowHeight / 2 - rightPaddle.rec.height / 2;
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        // draw the ball
+        DrawTextureRec(ball.texture, ball.rec, ball.pos, WHITE);
+        DrawTextureRec(leftPaddle.texture, leftPaddle.rec, leftPaddle.pos, WHITE);
+        DrawTextureRec(rightPaddle.texture, rightPaddle.rec, rightPaddle.pos, WHITE);
+
+        EndDrawing();
+        
+    };
+
+    UnloadTexture(ball.texture);
+    UnloadTexture(leftPaddle.texture);
+    UnloadTexture(rightPaddle.texture);
+    CloseWindow();
 }
